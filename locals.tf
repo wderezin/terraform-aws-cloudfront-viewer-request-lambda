57396017579a05d293c13814358f810d6157b4d2
Walter Derezinski <walter@daringway.com>
2020-11-08 11:54:58 -0500

    init release

--- locals.tf
+++ locals.tf
@@ -3,6 +3,6 @@ locals {
   lambda_name = var.lambda_name
   tags        = var.tags
 
-  apex_domain_redirect = var.apex_domain_redirect
-  index_rewrite        = var.index_rewrite
+  apex_domain_redirect = tostring(var.apex_domain_redirect)
+  index_rewrite        = tostring(var.index_rewrite)
 }
