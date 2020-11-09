46a082239612cc273acf0d379731024416739af6
Walter Derezinski <walter@daringway.com>
2020-11-08 20:42:39 -0500

    init release

--- locals.tf
+++ locals.tf
@@ -7,8 +7,8 @@ locals {
   index_rewrite        = tostring(var.index_rewrite)
 
   config_json = jsonencode({
-    "apex_domain_redirect" : ${tostring(var.apex_domain_redirect)},
-    "index_rewrite" : ${tostring(var.index_rewrite)},
+    "apex_domain_redirect" : tostring(var.apex_domain_redirect),
+    "index_rewrite" : tostring(var.index_rewrite),
     "ghost_hostname" : "${var.ghost_hostname}"
   })
 
